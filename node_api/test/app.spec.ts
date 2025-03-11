d0ee556f37a38aa4f6c43b3c5f8bf9ea83d48a14
victor orlunda <vicorlunda@gmail.com>
2025-03-11 15:18:06 +0100

    create test suites for the API

--- node_api/test/app.spec.ts
+++ node_api/test/app.spec.ts
@@ -1,10 +1,49 @@
 import { app } from '../src/app';
-import { expect, describe, beforeAll, afterAll, it } from 'vitest';
+import { expect, describe, it } from 'vitest';
 
 describe('Subnet Culator Test Suite', () => {
-  it('correctly evaluates a properly formatted cidr address');
+  it('correctly evaluates a properly formatted cidr address', async () => {
+    const expectedData = {
+      cidr: '/24',
+      host: '192.168.1.10',
+      subnet: '255.255.255.0',
+      network: '192.168.1.0',
+      address: '192.168.1.10/24',
+      subnet_block_size: 256,
+      changing_octet_position: 4,
+    };
 
-  it(
-    'returns a validation error response for an improperly formatted cidr address'
-  );
+    const res = await app.request('/api/calculate', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ address: '192.168.1.10/24' }),
+    });
+
+    const data = await res.json();
+
+    expect(res.status).toBe(200);
+    expect(data.message).toBe('Request successful');
+    expect(data.data).toStrictEqual(expectedData);
+  });
+
+  it('returns a validation error response for an improperly formatted cidr address', async () => {
+    const expectedCause = [
+      {
+        path: 'address',
+        message: 'Invalid cidr',
+      },
+    ];
+
+    const res = await app.request('/api/calculate', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ address: '192.168.1.10/33' }), // /33 is an out of range CIDR
+    });
+
+    const data = await res.json();
+
+    expect(res.status).toBe(422);
+    expect(data.message).toBe('Improperly formatted request');
+    expect(data.cause).toStrictEqual(expectedCause);
+  });
 });
